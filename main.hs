a169df44e1606ff86e2bb3f57257d6e73c5ae190
AdeAzhar <azharilkom24@gmail.com>
2026-06-28 14:51:23 +0700

    Feat: add task, delete task, show task

---
+++ main.hs
@@ -0,0 +1,62 @@
+module Main where
+
+import System.IO (hFlush, stdout)
+import Text.Read (readMaybe)
+import ToDoListManager
+import ToDoListManager (addTask)
+
+prompt :: String -> IO String
+prompt text = do
+    putStr text
+    hFlush stdout
+    getLine
+
+readInt ::String -> Maybe Int
+readInt = readMaybe
+
+mainLoop :: ToDoListManager -> IO ()
+mainLoop currentTaskList = do
+    putStrLn "\n=== TO-DO LIST MANAGER ==="
+    putStrLn "1. Add Task"
+    putStrLn "2. Delete Task"
+    putStrLn "3. Show Task List" 
+    putStrLn "4. Exit"
+
+    option <- prompt "Choose menu : "
+    case option of
+        "1" -> do
+            desc <- prompt "Add new task to your list : "
+            if null desc
+                then putStrLn "Cannot add task!" >> mainLoop currentTaskList
+                else do
+                    let updatedList = addTask desc currentTaskList
+                    putStrLn "Task successfully added!"
+                    mainLoop updatedList
+
+        "2" -> do
+            idStr <- prompt "Enter the ID of the task you want to delete : "
+            case readInt idStr of
+                Just targetTaskId -> do
+                    let updatedList = deleteTask targetTaskId currentTaskList
+                    putStrLn "Task successfully deleted!"
+                    mainLoop updatedList
+                Nothing -> do
+                    putStrLn "Invalid ID!"
+                    mainLoop currentTaskList
+        
+        "3" -> do
+            putStrLn "Task List"
+            putStrLn (showTask currentTaskList)
+            mainLoop currentTaskList
+
+        "4" -> putStrLn "See you!"
+
+        _ -> do
+            putStrLn "Invalid option!"
+            mainLoop currentTaskList
+
+main :: IO ()
+main = mainLoop emptyTaskList
+
+        
+
---
+++ todolistmanager.hs
@@ -0,0 +1,41 @@
+module ToDoListManager (
+    Task(..),
+    ToDoListManager,
+    emptyTaskList,
+    addTask,
+    deleteTask,
+    showTask
+) where
+
+import Data.List (intercalate)
+
+data Task = Task{
+    taskId :: Int,
+    taskDesc :: String,
+    isTaskCompleted :: Bool
+} deriving (Show, Eq)
+
+type ToDoListManager = [Task]
+
+emptyTaskList :: ToDoListManager
+emptyTaskList = []
+
+addTask :: String -> ToDoListManager -> ToDoListManager
+addTask desc currentTaskList =
+    let nextId = if null currentTaskList then 1 else maximum (map taskId currentTaskList) + 1
+        newTask = Task {taskId = nextId, taskDesc = desc, isTaskCompleted = False}
+    in currentTaskList ++ [newTask]
+
+deleteTask :: Int -> ToDoListManager -> ToDoListManager
+deleteTask targetTaskId currentTaskList =
+    filter (\t -> taskId t /= targetTaskId) currentTaskList
+
+showTask :: ToDoListManager -> String
+showTask [] = "Belum ada tugas yang ditambahkan"
+showTask list = 
+    let tableHeader = "-----------------------------------\nID  | Description | Status\n-----------------------------------"
+        renderStatus t = if isTaskCompleted t then "Finished" else "Not Finished Yet"
+        renderRow t = show (taskId t) ++ "   | " ++ taskDesc t ++ "  | " ++ renderStatus t 
+        rows = map renderRow list
+        tableFooter = "-----------------------------------"
+    in intercalate "\n" (tableHeader : rows ++ [tableFooter])
